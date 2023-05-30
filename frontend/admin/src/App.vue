4fb3430ca3b7f1805f8f61aa38e0c7df50365b3e
j-irion <juliusirion@gmx.de>
2023-05-30 12:41:23 +0200

    Implement and style client list

--- frontend/admin/src/App.vue
+++ frontend/admin/src/App.vue
@@ -1,30 +1,100 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+	<div id="app">
+		<div class="container" style="margin: 0; height: 100%">
+			<div class="row" style="height: 100%">
+				<div class="col-3" style="height: 100%">
+					<ClientList
+						:clients="clients"
+						@delete-client="deleteClient"
+					></ClientList>
+				</div>
+				<div class="col-9"></div>
+			</div>
+		</div>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import ClientList from "./components/ClientList.vue";
+
+export default {
+	components: {
+		ClientList,
+	},
+	data() {
+		return {
+			clients: [
+				{
+					id: 1,
+					name: "Peter Pan",
+					location: "Germany",
+					status: "busy",
+				},
+				{
+					id: 2,
+					name: "Alice Smith",
+					location: "United States",
+					status: "available",
+				},
+				{
+					id: 3,
+					name: "Juan Rodriguez",
+					location: "Spain",
+					status: "busy",
+				},
+				{
+					id: 4,
+					name: "Emily Chen",
+					location: "Canada",
+					status: "available",
+				},
+				{
+					id: 5,
+					name: "John Johnson",
+					location: "United Kingdom",
+					status: "busy",
+				},
+				{
+					id: 6,
+					name: "Sophia Lee",
+					location: "South Korea",
+					status: "available",
+				},
+				{
+					id: 7,
+					name: "Mohammed Ali",
+					location: "Saudi Arabia",
+					status: "busy",
+				},
+				{
+					id: 8,
+					name: "Maria Lopez",
+					location: "Mexico",
+					status: "available",
+				},
+				{
+					id: 9,
+					name: "Alexandre Silva",
+					location: "Brazil",
+					status: "busy",
+				},
+				{
+					id: 10,
+					name: "Elena Petrova",
+					location: "Russia",
+					status: "available",
+				},
+			],
+		};
+	},
+	methods: {
+		deleteClient(id) {
+			const index = this.clients.findIndex((obj) => obj.id === id);
+			if (index !== -1) {
+				this.clients.splice(index, 1);
+			}
+		},
+	},
+};
+</script>
+<style></style>
