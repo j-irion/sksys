089ee90393f27a60d3be54c5504b602cdeb2e5fd
j-irion <juliusirion@gmx.de>
2023-05-30 13:44:21 +0200

    Create NavBar with functionality

--- frontend/admin/src/App.vue
+++ frontend/admin/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div id="app">
+		<NavBar @add-client="addClient"></NavBar>
 		<div class="container" style="margin: 0; height: 100%">
 			<div class="row" style="height: 100%">
 				<div class="col-3" style="height: 100%">
@@ -16,71 +17,73 @@
 
 <script>
 import ClientList from "./components/ClientList.vue";
+import NavBar from "./components/Navbar.vue";
 
 export default {
 	components: {
 		ClientList,
+		NavBar,
 	},
 	data() {
 		return {
 			clients: [
 				{
 					id: 1,
-					name: "Peter Pan",
+					name: "Client 1",
 					location: "Germany",
 					status: "busy",
 				},
 				{
 					id: 2,
-					name: "Alice Smith",
+					name: "Client 2",
 					location: "United States",
 					status: "available",
 				},
 				{
 					id: 3,
-					name: "Juan Rodriguez",
+					name: "Client 3",
 					location: "Spain",
 					status: "busy",
 				},
 				{
 					id: 4,
-					name: "Emily Chen",
+					name: "Client 4",
 					location: "Canada",
 					status: "available",
 				},
 				{
 					id: 5,
-					name: "John Johnson",
+					name: "Client 5",
 					location: "United Kingdom",
 					status: "busy",
 				},
 				{
 					id: 6,
-					name: "Sophia Lee",
+					name: "Client 6",
 					location: "South Korea",
 					status: "available",
 				},
 				{
 					id: 7,
-					name: "Mohammed Ali",
+					name: "Client 7",
 					location: "Saudi Arabia",
 					status: "busy",
 				},
 				{
 					id: 8,
-					name: "Maria Lopez",
+					name: "Client 8",
 					location: "Mexico",
 					status: "available",
 				},
 				{
 					id: 9,
-					name: "Alexandre Silva",
+					name: "Client 9",
 					location: "Brazil",
 					status: "busy",
 				},
 				{
 					id: 10,
-					name: "Elena Petrova",
+					name: "Client 10",
 					location: "Russia",
 					status: "available",
 				},
@@ -93,6 +96,9 @@ export default {
 			if (index !== -1) {
 				this.clients.splice(index, 1);
 			}
+		},
+		addClient(newClient) {
+			this.clients.push(newClient);
 		},
 	},
 };
---
+++ frontend/admin/src/components/NavBar.vue
@@ -0,0 +1,72 @@
+<template>
+	<nav class="navbar">
+		<div class="container-fluid border-bottom pb-2">
+			<div class="navbar-brand">Admin Dashboard</div>
+			<form class="d-flex justify-content-center align-items-center">
+				<input
+					type="text"
+					class="form-control me-2"
+					placeholder="Name"
+					v-model="newClient.name"
+					required
+				/>
+				<input
+					type="text"
+					class="form-control me-2"
+					placeholder="Location"
+					v-model="newClient.location"
+					required
+				/>
+				<input
+					type="text"
+					class="form-control me-2"
+					placeholder="Status"
+					v-model="newClient.status"
+					required
+				/>
+				<button
+					class="btn btn-primary"
+					id="btn-add-client"
+					@click.prevent="addClient"
+				>
+					Add Client
+				</button>
+			</form>
+		</div>
+	</nav>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			newClient: {
+				name: "",
+				location: "",
+				status: "",
+			},
+		};
+	},
+	methods: {
+		addClient() {
+			if (
+				this.newClient.name === "" ||
+				this.newClient.location === "" ||
+				this.newClient.status === ""
+			)
+				return;
+			console.log(this.newClient);
+			this.$emit("add-client", this.newClient);
+			this.newClient = {
+				name: "",
+				location: "",
+				status: "",
+			};
+		},
+	},
+};
+</script>
+<style>
+#btn-add-client {
+	white-space: nowrap;
+}
+</style>
