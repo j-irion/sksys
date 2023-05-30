1de2897ebc435a6387f17107c346a8ecbaa32cf7
Robert Oleynik <r.oleynik@campus.tu-berlin.de>
2023-05-30 11:57:49 +0200

    feat(infra): setup initial dev env

---
+++ infra/dev/main.tf
@@ -0,0 +1,50 @@
+terraform {
+	required_providers {
+		docker = {
+			source = "kreuzwerker/docker"
+			version = "3.0.2"
+		}
+		random = {
+			source = "hashicorp/random"
+			version = "3.5.1"
+		}
+	}
+}
+
+provider "docker" {}
+provider "random" {}
+
+resource "random_password" "auth_db_password" {
+	length = 24
+}
+
+resource "random_password" "timeseries_db_password" {
+	length = 24
+}
+
+module "auth_db" {
+	source = "../modules/postgres"
+
+	name = "auth"
+	db = "auth"
+	user = "auth"
+	password = random_password.auth_db_password.result
+	mount_path = "./dev/auth"
+}
+
+module "timeseries_db" {
+	source = "../modules/influxdb"
+
+	name = "timeseries"
+	user = "time"
+	password = random_password.timeseries_db_password.result
+
+	org = "sksys"
+	bucket = "sksys"
+}
+
+module "dashboard" {
+	source = "../modules/grafana"
+
+	name = "dashboard"
+}
