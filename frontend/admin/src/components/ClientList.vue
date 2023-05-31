55b204c8a622cfcced797f2c5044591d8cec925c
Robert Oleynik <r.oleynik@campus.tu-berlin.de>
2023-05-31 14:33:12 +0200

    chore: update packages

--- frontend/admin/src/components/ClientList.vue
+++ frontend/admin/src/components/ClientList.vue
@@ -1,13 +1,18 @@
 <template>
-	<div class="overflow-scroll" style="height: 100%;">
+	<div class="overflow-scroll" style="height: 100%">
 		<div v-for="client in clients" class="card mt-2 mb-2">
 			<div class="card-body">
 				<h5 class="card-title">{{ client.name }}</h5>
-				<p class="card-text">
+				<div class="card-text">
 					<div><b>Location:</b> {{ client.location }}</div>
 					<div><b>Status:</b> {{ client.status }}</div>
-				</p>
-				<button class="btn btn-primary" @click="$emit('delete-client', client.id)">Remove</button>
+				</div>
+				<button
+					class="btn btn-primary"
+					@click="$emit('delete-client', client.id)"
+				>
+					Remove
+				</button>
 			</div>
 		</div>
 	</div>
