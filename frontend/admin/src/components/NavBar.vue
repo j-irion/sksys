0b78c8b6cc2d8221ced960bdb19901a62bec8263
j-irion <juliusirion@gmx.de>
2023-06-06 12:54:16 +0200

    Put add client form into modal

--- frontend/admin/src/components/NavBar.vue
+++ frontend/admin/src/components/NavBar.vue
@@ -2,38 +2,104 @@
 	<nav class="navbar bg-dark p-3">
 		<div class="container-fluid">
 			<div class="navbar-brand text-white fw-bold">Admin Dashboard</div>
-			<form class="d-flex justify-content-center align-items-center">
-				<input
-					type="text"
-					class="form-control me-2"
-					placeholder="Name"
-					v-model="newClient.name"
-					required
-				/>
-				<input
-					type="text"
-					class="form-control me-2"
-					placeholder="Location"
-					v-model="newClient.location"
-					required
-				/>
-				<input
-					type="text"
-					class="form-control me-2"
-					placeholder="Status"
-					v-model="newClient.status"
-					required
-				/>
-				<button
-					class="btn btn-light"
-					id="btn-add-client"
-					@click.prevent="addClient"
-				>
-					Add Client
-				</button>
-			</form>
+			<button
+				class="btn btn-light"
+				id="btn-add-client"
+				data-bs-toggle="modal"
+				data-bs-target="#exampleModal"
+			>
+				Add Client
+			</button>
 		</div>
 	</nav>
+
+	<div
+		class="modal fade"
+		id="exampleModal"
+		data-bs-backdrop="static"
+		data-bs-keyboard="false"
+		tabindex="-1"
+		aria-labelledby="exampleModalLabel"
+		aria-hidden="true"
+	>
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h1 class="modal-title fs-5" id="exampleModalLabel">
+						New Client
+					</h1>
+					<button
+						type="button"
+						class="btn-close"
+						data-bs-dismiss="modal"
+						@click="clearModal"
+						aria-label="Close"
+					></button>
+				</div>
+				<div class="modal-body">
+					<form>
+						<div class="mb-3">
+							<label for="clientName" class="form-label"
+								>Name</label
+							>
+							<input
+								id="clientName"
+								type="text"
+								class="form-control"
+								v-model="newClient.name"
+								required
+							/>
+						</div>
+						<div class="mb-3">
+							<label for="clientLocation" class="form-label"
+								>Location</label
+							>
+							<input
+								id="clientLocation"
+								type="text"
+								class="form-control"
+								v-model="newClient.location"
+								required
+							/>
+						</div>
+						<div class="mb-3">
+							<label for="clientStatus" class="form-label"
+								>Status</label
+							>
+							<div class="input-group">
+								<input
+									id="clientStatus"
+									type="number"
+									class="form-control"
+									v-model="newClient.status"
+									required
+								/>
+								<span class="input-group-text">kWh</span>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<button
+						type="button"
+						class="btn btn-secondary"
+						data-bs-dismiss="modal"
+						@click="clearModal"
+					>
+						Close
+					</button>
+					<button
+						type="button"
+						class="btn btn-primary"
+						@click="addClient"
+						data-bs-dismiss="modal"
+					>
+						Create Client
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
@@ -42,7 +108,7 @@ export default {
 			newClient: {
 				name: "",
 				location: "",
-				status: "",
+				status: 0,
 			},
 		};
 	},
@@ -59,7 +125,14 @@ export default {
 			this.newClient = {
 				name: "",
 				location: "",
-				status: "",
+				status: 0,
+			};
+		},
+		clearModal() {
+			this.newClient = {
+				name: "",
+				location: "",
+				status: 0,
 			};
 		},
 	},
