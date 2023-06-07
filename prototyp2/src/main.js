1af73d236c99d5b3bc2d0636b925317bcb10ec26
Robert Oleynik <r.oleynik@campus.tu-berlin.de>
2023-06-07 14:56:28 +0200

    style(client prototype): apply format

--- prototyp2/src/main.js
+++ prototyp2/src/main.js
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+const app = createApp(App);
 
+app.use(VueAxios, axios);
 
-const app = createApp(App)
-
-app.use(VueAxios, axios)
-
-app.mount('#app')
+app.mount("#app");
