e1b90a3b742e89fab43860b5aa228ab56acb63e3
Alexander Gaus <gaus@campus.tu-berlin.de>
2023-06-07 22:21:03 +0200

    status update

--- backend/ingester/src/main.rs
+++ backend/ingester/src/main.rs
@@ -1,4 +1,25 @@
+use axum::Router;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Deserialize, Serialize)]
+struct EnvConfig {}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+
+    let config: EnvConfig = match envy::from_env() {
+        Ok(config) => config,
+        Err(envy::Error::MissingValue(name)) => {
+            panic!("missing environment variable {}", name.to_uppercase())
+        }
+        Err(envy::Error::Custom(err)) => {
+            tracing::error!("{err}");
+            panic!()
+        }
+    };
+
+    let app = Router::new();
+    
 }
