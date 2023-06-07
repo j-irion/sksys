ecaa85ad42795b9c26442919699d682322798005
Pascal Reppert <pascal.reppert@campus.tu-berlin.de>
2023-06-07 08:47:04 +0000

    Prototyp2

--- Prototyp/script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("apiToken").addEventListener("input", function(e) {
-  let api = e.target.value;
-  document.getElementById("apiOutput").innerHTML = api;
-  
-});
---
+++ prototyp2/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">API Token</span>
+        <input id="apiToken" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      </div>
+      <button btn-primary btn-lg @click="intervall">weiter</button>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data: () => ({
+    count:1,
+  }),
+  
+  methods: {
+    intervall(){
+      setInterval(async () => {
+       await this.res();
+    }, 5000);
+  },
+   
+    async res(){
+
+      //let apiURL = "https://fakeapi.andreaspabst.com/api/todos"
+      let apiURL =  document.getElementById("apiToken").value;
+      
+      let response = await this.axios.get(apiURL);
+      await console.log(this.count , response);
+      this.count++;
+   
+    }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
