b4102f88b3b676e45c513b88fdc29e2cc26d2bbb
Robert Oleynik <r.oleynik@campus.tu-berlin.de>
2023-07-05 14:19:32 +0200

    style(client): apply format

--- frontend/client/src/App.vue
+++ frontend/client/src/App.vue
@@ -1,33 +1,37 @@
 <template>
 	<div class="container">
-		
-			<div class="input-group mb-3">
-				<span style="font-size: 19px;" class="input-group-text" id="inputGroup-sizing-default"
-					>API Token</span
-				>
-				<input
-					id="apiToken"
-					class="form-control"
-					aria-label="Sizing example input"
-					aria-describedby="inputGroup-sizing-default"
-				/>
-			</div>
+		<div class="input-group mb-3">
+			<span
+				style="font-size: 19px"
+				class="input-group-text"
+				id="inputGroup-sizing-default"
+				>API Token</span
+			>
+			<input
+				id="apiToken"
+				class="form-control"
+				aria-label="Sizing example input"
+				aria-describedby="inputGroup-sizing-default"
+			/>
+		</div>
 
-			<div class="input-group mb-3">
-				<label style="font-size: 15px;" class="input-group-text" for="metric">Stromeinheit:</label>
+		<div class="input-group mb-3">
+			<label style="font-size: 15px" class="input-group-text" for="metric"
+				>Stromeinheit:</label
+			>
 
-				<input
-					type="number"
-					id="metric"
-					name="metric"
-					class="form-control"
-					min="1"
-					max="1000000"
-				/>
-			</div>
-			<div>
-				<p>Sekunden bis zum nächsten Post: {{ timerCount }} </p>
-			</div>
+			<input
+				type="number"
+				id="metric"
+				name="metric"
+				class="form-control"
+				min="1"
+				max="1000000"
+			/>
+		</div>
+		<div>
+			<p>Sekunden bis zum nächsten Post: {{ timerCount }}</p>
+		</div>
 	</div>
 </template>
 
@@ -35,30 +39,28 @@
 export default {
 	name: "App",
 	data() {
-            return {
-                timerCount: 5
-            }
-        },
+		return {
+			timerCount: 5,
+		};
+	},
 
-        watch: {
-            timerCount: {
-                handler(value) {
+	watch: {
+		timerCount: {
+			handler(value) {
+				if (value > -1) {
+					setTimeout(() => {
+						this.timerCount--;
+					}, 1000);
+				}
+				if (value == -1) {
+					this.timerCount = 5;
+					this.res();
+				}
+			},
+			immediate: true,
+		},
+	},
 
-                    if (value > -1) {
-                        setTimeout(() => {
-                            this.timerCount--;
-                        }, 1000);
-                    }
-					if (value == -1) {	
-						this.timerCount = 5;
-						this.res();
-					}
-                },
-                immediate: true 
-            }
-
-        },
-		
 	methods: {
 		async res() {
 			const apiToken = document.getElementById("apiToken").value;
@@ -99,5 +101,4 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 }
-
 </style>
