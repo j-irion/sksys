7f6e48809b654ed81b35cf073f18eba647e98bc6
Robert Oleynik <r.oleynik@campus.tu-berlin.de>
2023-07-07 12:33:43 +0200

    feat: update client

--- scripts/fake-client.sh
+++ scripts/fake-client.sh
@@ -7,7 +7,7 @@ set -xeo pipefail
 
 COUNTER=1
 while true; do
-	VALUE=$(awk "BEGIN { print sin($COUNTER * 0.1) * 10 }")
+	VALUE=$(awk "BEGIN { print sin($COUNTER * 0.01) * 10 + 10 }")
 	curl -v "$HOST/api/submit" \
 		-H 'Content-Type: application/json' \
 		--data "{\"authtoken\":\"$TOKEN\",\"timestamp\":$(date "+%s"),\"used_power\":$VALUE}"
