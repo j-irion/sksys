3b399556898b6b07cfbd7b3ebecb3ecc0c33c334
Robert Oleynik <r.oleynik@campus.tu-berlin.de>
2023-07-08 14:10:19 +0200

    fix(client): vite config

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -4,10 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-});
-
-module.exports = {
 	server: {
 		base: "/client",
 	},
-};
+});
