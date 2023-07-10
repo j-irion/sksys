cfd9d71e9240c8a770c5fd4ae52f8fd929f3d1dc
David Celik <celik.david@hotmail.de>
2023-07-10 19:18:56 +0200

    Footprint and refreshrate 1h -> 5s

--- infra/modules/grafana-config/main.tf
+++ infra/modules/grafana-config/main.tf
@@ -82,13 +82,29 @@ resource "grafana_dashboard" "main" {
 							uid = grafana_data_source.data.uid
 						}
 						query = <<EOQ
-						from(bucket: "${var.influxdb_bucket}")
-						|>range(start: -1d)
-						|>truncateTimeColumn(unit: 1h)
-						|>filter(fn: (r) => r["_measurement"] == "power_usage" or r["_measurement"] == "carbon_intensity")
-						|>pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
-						|>map(fn: (r) => ({r with Footprint: r["carbon_intensity"] * r["power_usage"]/1000.0 }))
-						|>drop(columns: ["cabon_intensity", "_start", "_stop", "power_usage"])
+						import "join"
+
+						locations = from(bucket: "${var.influxdb_bucket}")
+							|>range(start: v.timeRangeStart, stop: v.timeRangeStop)
+							|>filter(fn: (r) => r["_measurement"] == "carbon_intensity")
+							|>aggregateWindow(every: 15s, fn: mean)
+							|>fill(usePrevious: true)
+							|>group(columns: ["machine_id", "location"])
+
+						clients = from(bucket: "${var.influxdb_bucket}")
+							|>range(start: v.timeRangeStart, stop: v.timeRangeStop)
+							|>filter(fn: (r) => r["_measurement"] == "power_usage")
+							|>aggregateWindow(every: 15s, fn: mean)
+							|>group(columns: ["location"])
+
+						join.inner(
+							left: clients,
+							right: locations,
+							on: (l, r) => l._time == r._time,
+							as: (l, r) => ({l with _value: l._value * r._value / 1000.0})
+						)
+						|>filter(fn: (r) => r["_value"] >= 0.0)
+						|>group(columns: ["machine_id"])
 						EOQ
 					}
 				]
